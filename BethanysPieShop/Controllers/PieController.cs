61120a80820af1d7b75b5c6fc129a8ba805f6520
CodingArc4 <kmunib10@outlook.com>
2023-09-27 14:16:00 +0500

    added css styling to the razor pages

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -1,4 +1,5 @@
 ﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BethanysPieShop.Controllers
@@ -16,8 +17,8 @@ namespace BethanysPieShop.Controllers
 
         public IActionResult List()
         {
-            ViewBag.CurrentCategory = "Cheese Cake";
-            return View(_pieRepository.AllPies);
+            PieListViewModel piesListViewModel = new PieListViewModel(_pieRepository.AllPies,"Cheese cakes");
+            return View(piesListViewModel);
         }
     }
 }
